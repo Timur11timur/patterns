464758a25165f25548a1114d5bb10efd6b1083e8
Timur <t.fralik@aimprosoft.com>
2020-01-17 16:55:59 +0200

    Fix displaying on main page

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-$all = scandir('.');
-
 $top = "<!doctype html>
 <html lang=\"en\">
   <head>
@@ -14,18 +12,29 @@ $top = "<!doctype html>
     <title>Design Patterns</title>
   </head>
   <body><div class=\"container pt-4\">
-  <h1>Design Patterns</h1>
-  <div class=\"list-group list-group-horizontal\">";
-$bottom = "</div><script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\" integrity=\"sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n\" crossorigin=\"anonymous\"></script>
+  <h1>Design Patterns</h1>";
+$bottom = "<script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\" integrity=\"sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n\" crossorigin=\"anonymous\"></script>
     <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>
     <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>
   </body>
 </html>";
 
 echo $top;
+$all = scandir('.');
 foreach ($all as $item) {
     if(is_dir($item) && (strpos($item, '.') === false)) {
-        echo "<a href=\"" . $item ."\" target=\"_blank\" class=\"list-group-item \">" . ucfirst($item) . "</a>";
+        echo "<div class=\"card\"><div class=\"card-body\">
+                <h2>" . ucfirst($item) . "</h2>
+                <div class=\"list-group list-group-horizontal\">";
+
+        $allnested = scandir('./' . $item . '/');
+        foreach ($allnested as $nesteditem) {
+            if(is_dir('./' . $item . '/'. $nesteditem) && (strpos($nesteditem, '.') === false)) {
+                echo "<a href=\"" . $nesteditem ."\" target=\"_blank\" class=\"list-group-item \">" . ucfirst($nesteditem) . "</a>";
+            }
+        }
+
+        echo "</div></div></div>";
     }
 }
 echo $bottom;
